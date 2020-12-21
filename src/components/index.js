b96dec9f02e02b96026977d031b4f5ecc066bfbc
Mirija <mirija160798@gmail.com>
2020-12-21 22:57:51 +0100

    Updates

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Cards} from './Cards/Cards';
-export { default as Chart} from './Chart/Chart';
-export { default as Countries} from './Countries/Countries';
-export { default as Navigation} from './Navigation/Navigation';
--- src/pages/index.js
+++
@@ -1,2 +0,0 @@
-export { default as homePage} from './homePage';
-export { default as Statistics} from './Statistics';
